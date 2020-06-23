d3a8fb659e2c9d486fbc402cc01b252dab14b258
Petia Gospodinova <34002260+aquaruiz@users.noreply.github.com>
2020-06-23 17:08:24 +0300

    use Point class

--- Java-Core/Nakov-introprogramming/exam1/src/exam1/problem02/Main.java
+++ Java-Core/Nakov-introprogramming/exam1/src/exam1/problem02/Main.java
@@ -5,8 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	private static final String INPUT_FILE_PATH = "src" + File.separator 
@@ -20,7 +20,6 @@ public class Main {
 			+ "Problem2.out";
 	
 	private static final String BLOCK_LETTER = "x";
-	private static final String WALK_LETTER = "0";
 	private static final String START_POINT_LETTER = "*";
 	
 	public static void main(String[] args) throws IOException {
@@ -37,9 +36,7 @@ public class Main {
 	}
 
 	private static String[][] readMatrix(String[][] matrix, String inputFilePath) throws IOException, NumberFormatException, ArrayIndexOutOfBoundsException {
-		BufferedReader bReader = null;
-		try {
-			bReader = new BufferedReader(new FileReader(inputFilePath));
+		try (BufferedReader bReader = new BufferedReader(new FileReader(inputFilePath));){
 			String line = bReader.readLine();
 			matrix = new String[Integer.parseInt(line)][];
 
@@ -49,79 +46,67 @@ public class Main {
 				matrix[index++] = line.split("");
 				line = bReader.readLine();
 			}
-		} catch (IOException | NumberFormatException | ArrayIndexOutOfBoundsException e) {
-			throw e;
-		} finally {
-			if (bReader != null) {
-				bReader.close();
-			}
-		}
+		} 
 
 		return matrix;
 	}
 
 	private static int countStepsToExit(String[][] matrix) {
-		int[] startPoint = findStart(matrix);
+		Point startPoint = findStart(matrix);
 		
-		if (startPoint[0] == -1 || startPoint[1] == -1) {
+		if (startPoint.row == -1 || startPoint.col == -1) {
 			return -1;
 		}
 		
-		LinkedList<String> visitedPoints = new LinkedList<>();
-		checkPoint(startPoint[0], startPoint[1], visitedPoints, matrix, 0);
+		Queue<Point> visitedPoints = new LinkedList<Point>();
+		checkPoint(startPoint.row, startPoint.col, visitedPoints, matrix, 0);
 		
 		while (!visitedPoints.isEmpty()) {
-			int[] entry = Arrays.stream(visitedPoints.remove().split(" "))
-					.mapToInt(Integer::parseInt)
-					.toArray();
-			int pointX = entry[0];
-			int pointY = entry[1];
-			int steps = entry[2];
-			steps++;
+			Point currentPoint = visitedPoints.remove();
+			int pointX = currentPoint.row;
+			int pointY = currentPoint.col;
+			int steps = currentPoint.steps;
+
 			if (pointX == 0 || pointX == matrix.length-1
 				|| pointY == 0 || pointY == matrix.length-1) {
-				return steps;
+				return steps+1;
 			}
 			
-			checkPoint(pointX+1, pointY, visitedPoints, matrix, steps);
-			checkPoint(pointX-1, pointY, visitedPoints, matrix, steps);
-			checkPoint(pointX, pointY+1, visitedPoints, matrix, steps);
-			checkPoint(pointX, pointY-1, visitedPoints, matrix, steps);
+			checkPoint(pointX+1, pointY, visitedPoints, matrix, steps+1);
+			checkPoint(pointX-1, pointY, visitedPoints, matrix, steps+1);
+			checkPoint(pointX, pointY+1, visitedPoints, matrix, steps+1);
+			checkPoint(pointX, pointY-1, visitedPoints, matrix, steps+1);
 		}
 		
-		int minSteps = 0;
-		return minSteps;
+		return -1;
 	}
 
-	private static void checkPoint(int pointX, int pointY, LinkedList<String> visitedPoints, String[][] matrix, int steps) {
+	private static void checkPoint(int pointX, int pointY, Queue<Point> visitedPoints, String[][] matrix, int steps) {
 		if (!matrix[pointX][pointY].equals(BLOCK_LETTER)) {
-			visitedPoints.add(String.format("%s %s %S", pointX, pointY, steps));
+			matrix[pointX][pointY] = BLOCK_LETTER;
+			Point point = new Point(pointX, pointY, steps);
+			visitedPoints.add(point);
 		}
 	}
 
-	private static int[] findStart(String[][] matrix) {
+	private static Point findStart(String[][] matrix) {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
 				if (matrix[i][j].equals(START_POINT_LETTER)) {
-					return new int[] {i, j};
+					return new Point(i, j, 0);
 				}
 			}
 		}
 		
-		return new int[] {-1, -1};
+		return new Point(-1, -1, 0);
 	}
 
 	private static void saveOutputResult(int output, String filePath) {
-		PrintWriter printWriter = null;
-		try {
-			printWriter = new PrintWriter(RESULT_FILE_PATH);
+		try (PrintWriter printWriter = new PrintWriter(RESULT_FILE_PATH);) {
 			printWriter.println(output);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if (printWriter != null) {
-				printWriter.close();
-			}
-		}
+		} 
 	}
 }
+	
