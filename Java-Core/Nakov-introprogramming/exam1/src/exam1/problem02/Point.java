cc51325682cdf8a455dc54c2b0f0645d26e13e3f
aquaruiz <gpetiaa@gmail.com>
2020-06-23 17:07:23 +0300

    add class Point

---
+++ Java-Core/Nakov-introprogramming/exam1/src/exam1/problem02/Point.java
@@ -0,0 +1,13 @@
+package exam1.problem02;
+
+public class Point {
+    int row;
+    int col;
+    int steps;
+    
+	public Point(int row, int col, int steps) {
+	    this.row = row;
+	    this.col = col;
+	    this.steps = steps;
+	}
+}
