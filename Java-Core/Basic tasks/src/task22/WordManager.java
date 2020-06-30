c914c6b1823db7b7f16b3f0f826e48f6128b5a0c
aquaruiz <gpetiaa@gmail.com>
2020-06-30 15:52:22 +0300

    task 2 b

--- Java-Core/Basic tasks/src/task22/WordManager.java
+++ Java-Core/Basic tasks/src/task22/WordManager.java
@@ -1,6 +1,8 @@
 package task22;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class WordManager {
 	private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -36,5 +38,27 @@ public class WordManager {
 		}
 		
 		return false;
+	}
+	
+	public static Character[] getSameChars(String[] randomWords) {
+		Set<Character> letters = new HashSet<>();
+		String firstWord = randomWords[0];
+		
+		Set<Character> secondeLetters = new HashSet<>();
+
+		String secondWord = randomWords[1];
+		
+		for (int i = 0; i < firstWord.length(); i++) {
+			letters.add(firstWord.charAt(i));
+		}
+		
+		for (int i = 0; i < secondWord.length(); i++) {
+			secondeLetters.add(secondWord.charAt(i));
+		}
+		
+		Character[] result = letters.stream()
+				.filter(x -> secondeLetters.contains(x))
+				.toArray(Character[]::new);
+		return result;
 	}
 }
