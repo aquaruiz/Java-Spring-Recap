8275530d5b2119f0819abac137047e2d73a0415b
aquaruiz <gpetiaa@gmail.com>
2020-06-30 17:04:03 +0300

    task 2 c

--- Java-Core/Basic tasks/src/task22/Main.java
+++ Java-Core/Basic tasks/src/task22/Main.java
@@ -6,31 +6,36 @@ import java.util.stream.Collectors;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] words = WordManager.generateRandomWordsWithLength(2, 5);
-		System.out.println(String.join(", ", words));
+		String[] words;
+//		words = WordManager.generateRandomWordsWithLength(2, 5);
+//		System.out.println(String.join(", ", words));
 		
 		/*
 		 * Check the generated words if they contain the letter 'g' 
 		 * ignoring case sensitivity and output the result for N = 5.
 		 */
-		words = WordManager.generateRandomWordsWithLength(2, 5);
-
-		System.out.println(String.join(", ", words) + " - Have g inside: " + WordManager.hasLetterInWords(words, 'g'));
+//		words = WordManager.generateRandomWordsWithLength(2, 5);
+//		System.out.println(String.join(", ", words) + " - Have g inside: " + WordManager.hasLetterInWords(words, 'g'));
 
 		/*
 		 * Compares the two words and return the characters that appear 
 		 * in both of the them for N = 20 honoring case sensitivity.
 		 */
-		words = WordManager.generateRandomWordsWithLength(2, 20);
-		
-		String bothChars = Arrays.stream(WordManager.getSameChars(words))
-				.map(x -> String.valueOf(x))
-				.collect(Collectors.joining(", "));
-		System.out.println(String.join(", ", words) + System.lineSeparator() + "Charactes in both: " + bothChars);
+//		words = WordManager.generateRandomWordsWithLength(2, 20);
+//		
+//		String bothChars = Arrays.stream(WordManager.getSameChars(words))
+//				.map(x -> String.valueOf(x))
+//				.collect(Collectors.joining(", "));
+//		System.out.println(String.join(", ", words) + System.lineSeparator() + "Charactes in both: " + bothChars);
 
 		/*
 		 * Identify the longest matching equal sequence of characters contained 
 		 * in both words irregardless of position for N = 50 000 and output it.
 		 */
+	
+		words = WordManager.generateRandomWordsWithLength(2, 5000);
+//		System.out.println(words[0].length());
+		
+		WordManager.getLongestMatchingSequence(words);
 	}
 }
--- Java-Core/Basic tasks/src/task22/WordManager.java
+++ Java-Core/Basic tasks/src/task22/WordManager.java
@@ -1,6 +1,8 @@
 package task22;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -44,7 +46,7 @@ public class WordManager {
 		Set<Character> letters = new HashSet<>();
 		String firstWord = randomWords[0];
 		
-		Set<Character> secondeLetters = new HashSet<>();
+		Set<Character> secondLetters = new HashSet<>();
 
 		String secondWord = randomWords[1];
 		
@@ -53,12 +55,70 @@ public class WordManager {
 		}
 		
 		for (int i = 0; i < secondWord.length(); i++) {
-			secondeLetters.add(secondWord.charAt(i));
+			secondLetters.add(secondWord.charAt(i));
 		}
 		
 		Character[] result = letters.stream()
-				.filter(x -> secondeLetters.contains(x))
+				.filter(x -> secondLetters.contains(x))
 				.toArray(Character[]::new);
+		
 		return result;
+	}
+
+	public static void getLongestMatchingSequence(String[] words) {
+		int[][] matrix = calcLCSMatrix(words);
+		List<String> subseq = restoreSubsequences(matrix, words);
+		System.out.println(matrix[matrix.length-1][matrix[0].length-1]);
+		System.out.println(String.join("", subseq));
+	}
+
+	private static List<String> restoreSubsequences(int[][] matrix, String[] words) {
+		char[] firstWord = words[0].toCharArray();
+		char[] secondWord = words[1].toCharArray();
+
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		int i = firstWord.length;
+		int j = secondWord.length;
+		
+		while (i > 0 && j > 0) {
+			if (firstWord[i-1] == secondWord[j-1]) {
+				stringBuilder.append(firstWord[i-1]);
+				i--;
+				j--;
+			} else if (matrix[i-1][j] > matrix[i][j-1]) {
+				i--;
+			} else {
+				j--;
+			}
+		}
+		
+		List<String> result = new ArrayList<>();
+		result.add(stringBuilder.reverse().toString());
+		return result;
+	}
+
+	private static int[][] calcLCSMatrix(String[] words) {
+		char[] firstWord = words[0].toCharArray();
+		char[] secondWord = words[1].toCharArray();
+		
+		int[][] matrix = new int[firstWord.length + 1][secondWord.length + 1];
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (i == 0 
+					|| j == 0) {
+				
+					matrix[i][j] = 0;
+				} else if (firstWord[i-1] == secondWord[j-1]) {
+					matrix[i][j] = matrix[i-1][j-1] + 1;					
+				} else {
+					matrix[i][j] = Math.max(matrix[i-1][j], matrix[i][j-1]);
+					
+				}
+			}
+		}
+
+		return matrix;
 	}
 }
