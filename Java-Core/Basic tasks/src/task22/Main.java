2d090abf19db52f02685c79bd61c1fa5460d4e3c
aquaruiz <gpetiaa@gmail.com>
2020-06-30 17:26:00 +0300

    arrange code

--- Java-Core/Basic tasks/src/task22/Main.java
+++ Java-Core/Basic tasks/src/task22/Main.java
@@ -7,26 +7,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		String[] words;
-//		words = WordManager.generateRandomWordsWithLength(2, 5);
-//		System.out.println(String.join(", ", words));
+		words = WordManager.generateRandomWordsWithLength(2, 5);
+		System.out.println(String.join(", ", words));
 		
 		/*
 		 * Check the generated words if they contain the letter 'g' 
 		 * ignoring case sensitivity and output the result for N = 5.
 		 */
-//		words = WordManager.generateRandomWordsWithLength(2, 5);
-//		System.out.println(String.join(", ", words) + " - Have g inside: " + WordManager.hasLetterInWords(words, 'g'));
+		words = WordManager.generateRandomWordsWithLength(2, 5);
+		System.out.println(String.join(", ", words) + " - Have g inside: " + WordManager.hasLetterInWords(words, 'g'));
 
 		/*
 		 * Compares the two words and return the characters that appear 
 		 * in both of the them for N = 20 honoring case sensitivity.
 		 */
-//		words = WordManager.generateRandomWordsWithLength(2, 20);
-//		
-//		String bothChars = Arrays.stream(WordManager.getSameChars(words))
-//				.map(x -> String.valueOf(x))
-//				.collect(Collectors.joining(", "));
-//		System.out.println(String.join(", ", words) + System.lineSeparator() + "Charactes in both: " + bothChars);
+		words = WordManager.generateRandomWordsWithLength(2, 20);
+		
+		String bothChars = Arrays.stream(WordManager.getSameChars(words))
+				.map(x -> String.valueOf(x))
+				.collect(Collectors.joining(", "));
+		System.out.println(String.join(", ", words) + System.lineSeparator() + "Charactes in both: " + bothChars);
 
 		/*
 		 * Identify the longest matching equal sequence of characters contained 
@@ -34,7 +34,7 @@ public class Main {
 		 */
 	
 		words = WordManager.generateRandomWordsWithLength(2, 21000);
-//		System.out.println(words[0].length());
+		System.out.println(words[0].length());
 		
 		WordManager.getLongestMatchingSequence(words);
 	}
