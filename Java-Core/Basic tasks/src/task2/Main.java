e8a198b160a6da75d1c6ce0983d1cd061ef343aa
aquaruiz <gpetiaa@gmail.com>
2020-06-30 15:43:35 +0300

    task 2

--- Java-Core/Basic tasks/src/task2/Main.java
+++ Java-Core/Basic tasks/src/task2/Main.java
@@ -6,14 +6,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
