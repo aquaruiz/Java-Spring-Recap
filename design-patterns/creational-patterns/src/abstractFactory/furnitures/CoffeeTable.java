f7449f94a5602639a24889aedd7494c313e58a0e
aquaruiz <gpetiaa@gmail.com>
2020-07-09 18:28:07 +0300

    furniture  factory

---
+++ design-patterns/creational-patterns/src/abstractFactory/furnitures/CoffeeTable.java
@@ -0,0 +1,6 @@
+package abstractFactory.furnitures;
+
+public interface CoffeeTable {
+	boolean isTall();
+	void ServeOnIt();
+}
---
+++ design-patterns/creational-patterns/src/abstractFactory/furnitures/impl/VictorianCofeeTable.java
@@ -0,0 +1,15 @@
+package abstractFactory.furnitures.impl;
+
+import abstractFactory.furnitures.CoffeeTable;
+
+public class VictorianCofeeTable implements CoffeeTable{
+
+	public boolean isTall() {
+		return false;
+	}
+
+	public void ServeOnIt() {
+		// TODO Auto-generated method stub
+	}
+
+}
