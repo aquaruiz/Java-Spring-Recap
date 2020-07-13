a5bc283fc94ab03206087b40a9db2e0254a49357
aquaruiz <gpetiaa@gmail.com>
2020-07-13 16:21:01 +0300

    add second example of proxy pattern

---
+++ design-patterns/structural-patterns/src/proxy2/App.java
@@ -0,0 +1,15 @@
+package proxy2;
+
+public class App {
+	 public static void main (String[] args) { 
+		 
+        Internet internet = new ProxyInternet(); 
+        
+        try { 
+            internet.connectTo("abv.org"); 
+            internet.connectTo("abc.com"); 
+        } catch (Exception e) { 
+            System.out.println(e.getMessage()); 
+        } 
+    } 
+}
---
+++ design-patterns/structural-patterns/src/proxy2/RealInternet.java
@@ -0,0 +1,10 @@
+package proxy2;
+
+public class RealInternet implements Internet {
+
+	@Override
+	public void connectTo(String serverhost) throws Exception {
+        System.out.println("Connecting to "+ serverhost); 		
+	}
+
+}
