b9089a323d1bbee67e98459218d4058eae0d856c
aquaruiz <gpetiaa@gmail.com>
2020-07-15 19:40:28 +0300

    remove extra-added method

--- design-patterns/behavioral-patterns/src/observer/Surveillance.java
+++ design-patterns/behavioral-patterns/src/observer/Surveillance.java
@@ -4,11 +4,7 @@ class Surveillance implements AlarmListener {
 	public void alarm() {
 		System.out.println("Surveillance - by the numbers:");
 		System.out.println("   establish a perimeter");
-		isolate();
-		System.out.println("   identify the source");
-	}
-
-	protected void isolate() {
 		System.out.println("   train the cameras");
+		System.out.println("   identify the source");
 	}
 }
