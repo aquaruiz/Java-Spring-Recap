1902b992414edc6355ce8c5d89751c4ccaa0f95e
Petia Gospodinova <34002260+aquaruiz@users.noreply.github.com>
2020-07-15 20:07:44 +0300

    add mediator (#16)

---
+++ design-patterns/behavioral-patterns/src/mediator/Button.java
@@ -0,0 +1,10 @@
+package mediator;
+
+public class Button extends Component {
+
+	public Button(Mediator dialog) {
+		super(dialog);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ design-patterns/behavioral-patterns/src/mediator/Component.java
@@ -0,0 +1,17 @@
+package mediator;
+
+public class Component {
+	protected Mediator dialog;
+
+	public Component(Mediator dialog) {
+		this.dialog = dialog;
+	}
+
+	public void click() {
+		dialog.notify(this, "click");
+	}
+
+	public void keypress() {
+		dialog.notify(this, "keypress");
+	}
+}
