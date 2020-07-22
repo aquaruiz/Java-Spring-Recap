beee718a9db594e6d43d77d187c297a0a81ab256
Petia Gospodinova <34002260+aquaruiz@users.noreply.github.com>
2020-07-22 15:24:49 +0300

    MySQL (#26)
    * create table
    
    * rearrange files
    
    * create tables
    
    * populate data
    
    * add procedures
    
    * fix in lower and upper case and remove snake naming
    
    * add DB diagram
    
    * rename id column in details table
    
    * redesign DB
    
    * edit sql according new diagram
    
    * new diagram
    
    * edit log tables and log procedures
    
    * add new diagram
    
    * remove unused view and fix mistakes
    
    * fix code style

--- MySQL/Scripts/Script.sql
+++ MySQL/Scripts/Script.sql
@@ -8,16 +8,10 @@ CREATE TABLE product(
 	id INT NOT NULL AUTO_INCREMENT UNIQUE,
 	name VARCHAR(255) NOT NULL,
 	shortDescription TEXT,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE productDetail(
-	productId INT NOT NULL UNIQUE,
 	weight FLOAT DEFAULT 0.00,
 	barcodeNumber VARCHAR(15),
 	price DECIMAL(5, 2),
-	PRIMARY KEY (productId),
-	FOREIGN KEY (productId) REFERENCES product(id)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE customer (
@@ -25,16 +19,10 @@ CREATE TABLE customer (
 	financialId VARCHAR(255),
 	isVatRegistered BOOLEAN DEFAULT FALSE,
 	vatNumber VARCHAR(255),
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE customerDetail (
-	customerId INT NOT NULL UNIQUE,
 	name VARCHAR(255) NOT NULL,
 	address TEXT NOT NULL,
 	contactPerson VARCHAR(255),
-	PRIMARY KEY (customerId),
-	FOREIGN KEY (customerId) REFERENCES customer(id)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE customerOrder (
@@ -57,49 +45,54 @@ CREATE TABLE productOrder (
 	FOREIGN KEY (orderId) REFERENCES customerOrder(id)
 );	
 
-CREATE TABLE activityLog (
+CREATE TABLE customerActivityLog (
 	id INT NOT NULL AUTO_INCREMENT UNIQUE,
-	username VARCHAR(255) DEFAULT 'Anonymous',
+	modifiedAt DATETIME DEFAULT CURRENT_TIMESTAMP(),
+	modifiedBy VARCHAR(255) DEFAULT 'Anonymous',
+	eventType VARCHAR(255),
+	customerId INT NOT NULL,
 	description TEXT,
-	editedCustomerId INT DEFAULT NULL,
-	editedProductId INT DEFAULT NULL,
-	editedOrderId INT DEFAULT NULL,
-	dateTime DATETIME DEFAULT CURRENT_TIMESTAMP(),
 	PRIMARY KEY (id),
-	FOREIGN KEY (editedCustomerId) REFERENCES customer(id),
-	FOREIGN KEY (editedProductId) REFERENCES product(id),
-	FOREIGN KEY (editedOrderId) REFERENCES customerOrder(id)
+	FOREIGN KEY (customerId) REFERENCES customer(id)
+);
+
+CREATE TABLE productActivityLog (
+	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+	modifiedAt DATETIME DEFAULT CURRENT_TIMESTAMP(),
+	modifiedBy VARCHAR(255) DEFAULT 'Anonymous',
+	eventType VARCHAR(255),
+	productId INT DEFAULT NULL,
+	description TEXT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (productId) REFERENCES product(id)
+);
+
+CREATE TABLE customerOrderActivityLog (
+	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+	modifiedAt DATETIME DEFAULT CURRENT_TIMESTAMP(),
+	modifiedBy VARCHAR(255) DEFAULT 'Anonymous',
+	eventType VARCHAR(255),
+	customerOrderId INT DEFAULT NULL,
+	description TEXT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (customerOrderId) REFERENCES customerOrder(id)
 );
 
 -- populate data in Tables
 
-INSERT INTO product (id, name, shortDescription)
+INSERT INTO product (id, name, shortDescription, weight, barcodeNumber, price)
 VALUES 
-	(1, 'chair', 'one very nice chair'),
-	(2, 'table', 'very nice kitchen table'),
-	(3, 'wardrobe', 'really large wardrobe'),
-	(4, 'sofa', 'a comforTABLE sofa'),
-	(5, 'bed', 'one premium size bed');
+	(1, 'chair', 'one very nice chair', 4.850, '012356745650', 25.50),
+	(2, 'table', 'very nice kitchen table', 10.360, '012345675650', 225.50),
+	(3, 'wardrobe', 'really large wardrobe', 32.100, '012345674650', 485.50),
+	(4, 'sofa', 'a comforTABLE sofa', 15.685, '012346745650', 350.99),
+	(5, 'bed', 'one premium size bed', 50.950, '012345674565', 860.99);
 
-INSERT INTO productDetail (produstId, weight, barcodeNumber, price)
+INSERT INTO customer (id, financialId, isVatRegistered, vatNumber, name, address, contactPerson)
 VALUES 
-	(1, 4.850, '012356745650', 25.50),
-	(2, 10.360, '012345675650', 225.50),
-	(3, 32.100, '012345674650', 485.50),
-	(4, 15.685, '012346745650', 350.99),
-	(5, 50.950, '012345674565', 860.99);
-
-INSERT INTO customer (id, financialId, isVatRegistered, vatNumber)
-VALUES 
-	(11, '', FALSE, NULL),
-	(12, '000459797', FALSE, NULL),
-	(13, NULL, TRUE, '584635416');
-
-INSERT INTO customerDetail (customerId, name, address, contactPerson)
-VALUES 
-	(11, 'Flowers LLC', 'Pleven, ul. Skopie 56', 'Jane Doe'),
-	(12, 'IBM LTD', 'Sofia, ul. Tsarigradsko shose 256', 'Anna Georgieva'),
-	(13, 'Evil Corp.', 'New York, bul. Bulgaria 001', NULL);
+	(11, '', FALSE, NULL, 'Flowers LLC', 'Pleven, ul. Skopie 56', 'Jane Doe'),
+	(12, '000459797', FALSE, NULL, 'IBM LTD', 'Sofia, ul. Tsarigradsko shose 256', 'Anna Georgieva'),
+	(13, NULL, TRUE, '584635416', 'Corp.', 'New York, bul. Bulgaria 001', NULL);
 
 INSERT INTO customerOrder (id,	invoiceNumber, orderDate, totalPrice, customerId)
 VALUES 
@@ -115,27 +108,9 @@ VALUES
 	(2, 102, '00005', 6),
 	(4, 102, '00005', 1);
 
-INSERT INTO activityLog (description, editedCustomerId, dateTime)
+INSERT INTO customerOrderActivityLog (description, customerOrderId, modifiedAt, eventType)
 VALUES 
-	('nothing happened', 12, CURRENT_TIMESTAMP());
-
--- CREATE some Views
-
-CREATE VIEW v_get_products_data AS
-	SELECT p.id, p.name, p.shortDescription, pd.weight, pd.price, pd.barcodeNumber 
-		FROM product AS p
-		JOIN productDetail AS pd
-		ON p.id = pd.productId;
-
-SELECT * FROM v_get_products_data; 
-
-CREATE VIEW v_get_customers_data AS
-	SELECT c.id, cd.name, c.financialId, c.vatNumber, cd.address, cd.contactPerson 
-		FROM customer AS c 
-		JOIN customerDetail AS cd 
-		ON c.id = cd.customerId;
-
-SELECT * FROM v_get_customers_data; 
+	('nothing happened', 102, CURRENT_TIMESTAMP(), 'edit');
 
 -- CREATE  search queries
 
@@ -147,12 +122,13 @@ CREATE PROCEDURE sp_get_product_info_by_part_product_name
    productName VARCHAR(255)
 ) 
 BEGIN 
-   SELECT * FROM get_products_data 
-   WHERE name LIKE CONCAT('%', productName, '%'); 
+   SELECT p.id, p.name, p.shortDescription, p.weight, p.price, p.barcodeNumber 
+		FROM product AS p
+   		WHERE p.name LIKE CONCAT('%', productName, '%'); 
 end$$
 DELIMITER;
 
-CALL get_product_info_by_part_product_name('hai'); 
+CALL sp_get_product_info_by_part_product_name('hai'); 
 
 -- DROP PROCEDURE sp_get_product_info_by_part_product_name;
 
@@ -161,23 +137,19 @@ CREATE PROCEDURE sp_get_order_info_by_order_number
    orderNumber INT
 ) 
 BEGIN  
-	SELECT po.orderId, c.vatNumber, cd.name, p.name, po.productQuantity, pd.price AS 'price per unit', pd.price * po.productQuantity AS 'total price' 
+	SELECT po.orderId, c.vatNumber, c.name, p.name, po.productQuantity, p.price AS 'price per unit', p.price * po.productQuantity AS 'total price' 
 		FROM productOrder AS po
 		JOIN product AS p
-		ON p.id = po.id 
-		JOIN productDetail AS pd
-		ON p.id = pd.productId 
+		ON p.id = po.productId 
 		JOIN customerOrder AS o
 		ON o.id = po.orderId 
 		JOIN customer AS c
 		ON c.id = o.customerId
-		JOIN customerDetail AS cd
-		ON cd.customerId  = c.id
 		WHERE po.orderId = orderNumber;
 END; 
 
 CALL sp_get_order_info_by_order_number(101);
-  
+
 -- DROP PROCEDURE sp_get_order_info_by_orderNumber;
 
 CREATE PROCEDURE sp_get_orders_for_last_month_by_customer_name
@@ -185,11 +157,11 @@ CREATE PROCEDURE sp_get_orders_for_last_month_by_customer_name
    customerName VARCHAR(255)
 ) 
 BEGIN
-	SELECT o.id AS 'ORDER id', o.totalPrice, o.orderDate, cd.name AS 'customer name'
+	SELECT o.id AS 'ORDER id', o.totalPrice, o.orderDate, c.name AS 'customer name'
 		FROM customerOrder AS o 
-		JOIN customerDetail AS cd
-		ON cd.id = o.customerId
-		WHERE cd.name LIKE CONCAT('%', customerName, '%') 
+		JOIN customer AS c
+		ON c.id = o.customerId
+		WHERE c.name LIKE CONCAT('%', customerName, '%') 
 		AND DATE_ADD(o.orderDate, INTERVAL 1 MONTH) > current_date();
 END;
 
@@ -200,14 +172,12 @@ CALL sp_get_orders_for_last_month_by_customer_name('Flower');
 CREATE VIEW sp_get_products_sold_last_month AS
 	SELECT p.name AS 'product.name', sum(po.productQuantity) AS 'total quantity sold last month' 
 		FROM productOrder AS po 
-		JOIN productDetail AS pd
-		ON pd.id = po.productId 
 		JOIN product AS p
-		ON p.id = pd.id 
+		ON po.productId = p.id 
 		JOIN customerOrder AS o
 		ON o.id = po.orderId 
 		WHERE DATE_ADD(o.orderDate, INTERVAL 1 Month) > current_date()
-		GROUP BY pd.id 
+		GROUP BY p.id 
 		ORDER BY p.name;
 
 SELECT * FROM sp_get_products_sold_last_month; 
@@ -216,62 +186,76 @@ SELECT * FROM sp_get_products_sold_last_month;
 
 -- add indices
 
-CREATE INDEX index_product
-ON productDetail (id);
+CREATE INDEX index_product_log
+ON productActivityLog (productId);
 
-CREATE INDEX indexOrder
-ON productOrder (orderId, productId);
+CREATE INDEX index_customer_order_log
+ON customerOrderActivityLog (customerOrderId);
+
+CREATE INDEX index_customer_log
+ON customerActivityLog (customerId);
 
 -- Log info in logs table
 
-CREATE PROCEDURE sp_insert_in_log_table(msg TEXT, customerId INT, productId INT, orderId INT)
+CREATE PROCEDURE sp_insert_in_product_log_table(eventType VARCHAR(255), msg TEXT, productId INT)
 BEGIN
-	IF customerId IS NOT NULL THEN 
-		IF (SELECT COUNT(*) FROM customer	
-				WHERE customer.id = customerId) > 0 THEN 
-        	INSERT INTO activityLog (description, editedCustomerId) VALUES (msg, customerId);
-        END IF;
-    END IF;
-   
 	IF productId IS NOT NULL THEN 
 		IF (SELECT COUNT(*) FROM product	
 				WHERE product.id = productId) > 0 THEN 
-       		INSERT INTO activityLog (description, editedProductId) VALUES (msg, productId);
+       		INSERT INTO productActivityLog (eventType, description, productId) VALUES (eventType, msg, productId);
        END IF;
-    END IF;
-   
-	IF orderId IS NOT NULL THEN 
-		IF (SELECT COUNT(*) FROM customerOrder
-				WHERE customerOrder.id = orderId) > 0 THEN 
-	        INSERT INTO activityLog (description, editedOrderId) VALUES (msg, orderId);
+    END IF;   
+END;
+
+
+CREATE PROCEDURE sp_insert_in_customer_order_log_table(eventType VARCHAR(255), msg TEXT, customerOrderId INT)
+BEGIN
+	IF customerOrderId IS NOT NULL THEN 
+		IF (SELECT COUNT(*) FROM customerOrder	
+				WHERE customerOrderId.id = orderId) > 0 THEN 
+       		INSERT INTO orderActivityLog (eventType, description, customerOrderId) VALUES (eventType, msg, customerOrderId);
        END IF;
     END IF;
 END;
 
--- DROP PROCEDURE sp_insert_in_log_table;
+CREATE PROCEDURE sp_insert_in_customer_log_table(eventType VARCHAR(255), msg TEXT, customerId INT)
+BEGIN	
+	IF customerId IS NOT NULL THEN 
+		IF (SELECT COUNT(*) FROM customer	
+				WHERE customer.id = customerId) > 0 THEN 
+       		INSERT INTO customerActivityLog (eventType, description, customerId) VALUES (eventType, msg, customerId);
+       END IF;
+    END IF;
+END;
 
-CALL sp_insert_in_log_table('log edited', 00000, 1, 00000);
+-- DROP PROCEDURE sp_insert_in_product_log_table;
+-- DROP PROCEDURE sp_insert_in_order_log_table;
+-- DROP PROCEDURE sp_insert_in_customer_log_table;
+
+CALL sp_insert_in_customer_log_table('EDIT', 'customer log edited', 13);
 
 -- insert product with transaction
 
 START TRANSACTION;
-	INSERT INTO product (id, name, shortDescription)
+	INSERT INTO product (id, name, shortDescription, weight, barcodeNumber, price)
 	VALUES 
-		(10, 'desk', 'a simple desk');
-	
-	INSERT INTO productDetail (productId, weight, barcodeNumber, price)
-	VALUES 
-		(10, 8.000, '012356745650', 140.50);
+		(10, 'desk', 'a simple desk', 8.000, '012356745650', 140.50);
+
+	CALL sp_insert_in_product_log_table('INSERT', "added new product", 10);
 COMMIT;
+
+-- DROP TABLE customerActivityLog;
+
+-- DROP TABLE customerOrderActivityLog;
+
+-- DROP TABLE productActivityLog;
+
+-- DROP TABLE productOrder;
+
+-- DROP TABLE customerOrder;
 
 -- DROP TABLE product;
 
 -- DROP TABLE customer;
-
--- DROP TABLE customerOrder;
-
--- DROP TABLE productOrder;
-
--- DROP TABLE activityLog;
 
 -- DROP DATABASE factory; 
