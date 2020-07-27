7460657ce4a2a6863d34178e8dbf6acbc944c45a
aquaruiz <gpetiaa@gmail.com>
2020-07-27 20:57:10 +0300

    add thymeleaf page

---
+++ Spring/mvc-conference-thymeleaf/src/main/java/com/mysite/mvcconference/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.mysite.mvcconference.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+	
+	@GetMapping("greeting")
+	public String greeting(Map<String, Object> model) {
+		model.put("message", "Hello Bryan");
+		return "greeting"; // returns jsp file name
+		
+	}
+	
+	@GetMapping("thyme")
+	public String thyme(Map<String, Object> model) {
+		model.put("message", "Hello THYMELEAF");
+		return "thyme"; // returns jsp file name
+		
+	}
+}
---
+++ Spring/mvc-conference-thymeleaf/src/test/java/com/mysite/mvcconference/MvcConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.mvcconference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
