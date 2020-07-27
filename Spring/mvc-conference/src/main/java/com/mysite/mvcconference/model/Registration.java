4a8538e0f665d941429ae5a892f53a4c5ea6df82
aquaruiz <gpetiaa@gmail.com>
2020-07-27 19:36:37 +0300

    add registration jsp with i18n support

---
+++ Spring/mvc-conference/src/main/java/com/mysite/mvcconference/model/Registration.java
@@ -0,0 +1,13 @@
+package com.mysite.mvcconference.model;
+
+public class Registration {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
