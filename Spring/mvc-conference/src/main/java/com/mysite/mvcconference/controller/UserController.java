a6762582deada4b0fd774e99df910db80ca6e0b9
aquaruiz <gpetiaa@gmail.com>
2020-07-28 12:55:46 +0300

    add jQuery

--- Spring/mvc-conference/src/main/java/com/mysite/mvcconference/controller/UserController.java
+++ Spring/mvc-conference/src/main/java/com/mysite/mvcconference/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mysite.mvcconference.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class UserController {
 		return user;
 	}
 
-	@GetMapping("/user")
+	@PostMapping("/user")
 	public User postUser(User user) {
 		return user;
 	}
