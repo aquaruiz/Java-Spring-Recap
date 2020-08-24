d3d507589fe1321bec4335942cd667819412f623
aquaruiz <gpetiaa@gmail.com>
2020-08-24 19:16:40 +0300

    .

--- Spring/devcamp-homeworks/src/main/resources/db/migration/V1_0_0__migration.sql
+++ Spring/devcamp-homeworks/src/main/resources/db/migration/V1_0_0__migration.sql
@@ -1,45 +1,53 @@
---CREATE TABLE IF NOT EXISTS `employee` (
---	 
---	    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
---	    `name` varchar(20),
---	    `email` varchar(50),
---	    `date_of_birth` timestamp
---	 
---	)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
---	
-create TABLE IF NOT EXISTS roles (
-       id bigint not null auto_increment,
-        role varchar(255),
-        primary key (id)
-    ) engine=InnoDB;
-    
-create TABLE IF NOT EXISTS students (
+create table students (
    id bigint not null auto_increment,
-    age integer not null,
+    age integer default 25,
     first_name varchar(50),
     last_name varchar(50),
-    primary key (id)
-) engine=InnoDB;
-    
-create TABLE IF NOT EXISTS teachers (
-   id bigint not null auto_increment,
-    fullname varchar(255),
-    subject varchar(255),
+    user_id bigint,
     primary key (id)
 ) engine=InnoDB;
 
-create table IF NOT EXISTS teachers_students (
-   student_id bigint not null,
-    teacher_id bigint not null,
-    primary key (student_id, teacher_id)
+create table teachers (
+   id bigint not null auto_increment,
+    fullname varchar(255),
+    subject varchar(255),
+    user_id bigint,
+    primary key (id)
 ) engine=InnoDB;
-    
- alter table teachers_students 
-   add constraint FKs9qt95drbmg1yu66e24njnxl0 
-   foreign key (teacher_id) 
-   references teachers (id)
-       
- alter table teachers_students 
-   add constraint FKr2jjh7fdvijix8q8hervypaes 
-   foreign key (student_id) 
-   references students (id)
+
+create table teachers_students (
+   students_id bigint not null,
+    teachers_id bigint not null,
+    primary key (students_id, teachers_id)
+) engine=InnoDB;
+
+create table users (
+   id bigint not null auto_increment,
+    password varchar(20) not null,
+    user_type integer,
+    username varchar(40) not null,
+    primary key (id)
+) engine=InnoDB;
+
+alter table users 
+   add constraint UK_r43af9ap4edm43mmtq01oddj6 unique (username);
+
+alter table students 
+   add constraint FKdt1cjx5ve5bdabmuuf3ibrwaq 
+   foreign key (user_id) 
+   references users (id);
+
+alter table teachers 
+   add constraint FKb8dct7w2j1vl1r2bpstw5isc0 
+   foreign key (user_id) 
+   references users (id);
+
+alter table teachers_students 
+   add constraint FK8oyy0mstwh9f9qgsedldsdcji 
+   foreign key (teachers_id) 
+   references teachers (id);
+
+alter table teachers_students 
+   add constraint FKtltfdvm0002nwfe2p12gltgb 
+   foreign key (students_id) 
+   references students (id);
